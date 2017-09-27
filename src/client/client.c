95fd718711c42fa92af2dc41c0d356dece381710
youngjune_park <youngjune_park2@tmax.co.kr>
2017-09-27 17:26:04 +0900

    While looping in client and server :)

--- src/client/client.c
+++ src/client/client.c
@@ -4,34 +4,43 @@
 #include <string.h>
 
 int main(){
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
+    int clientSocket;
+    char recv_buffer[1024];
+    char send_buffer[1024];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(7891);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(7891);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+    /*---- Connect the socket to the server using the address struct ----*/
+    addr_size = sizeof serverAddr;
 
-  /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+    while(1){
+        connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+        scanf("%s",send_buffer);
 
-  /*---- Print the received message ----*/
-  printf("Data received: %s",buffer);
+        /*---- Send the message from the user input into the send_buffer ----*/
+        send(clientSocket, send_buffer, 13,0);
+        printf("Data sended: %s\n", send_buffer);
 
-  return 0;
+        /*---- Read the message from the server into the recv_buffer ----*/
+        recv(clientSocket, recv_buffer, 1024, 0);
+        printf("Data received: %s\n",recv_buffer);
+
+        memset(send_buffer, '\0', 1024);
+        memset(recv_buffer, '\0', 1024);
+    }
+    return 0;
 }
